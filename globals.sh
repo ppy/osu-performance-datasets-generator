7e4aa065cc1cc0b03c6eda7e4434d77e791172d3
ThePooN <thepoon@osufr.live>
2022-06-30 01:08:13 +0200

    🔧 Set default values

--- globals.sh
+++ globals.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-DATABASE_HOST=db-delayed
-DATABASE_USER=performance-export
+DATABASE_HOST="${DATABASE_HOST:-db-delayed}"
+DATABASE_USER="${DATABASE_USER:-performance-export}"
+
+S3_BUCKET="${S3_BUCKET:-data.ppy.sh}"
 
 DATE=$(date +"%Y_%m_%d")
