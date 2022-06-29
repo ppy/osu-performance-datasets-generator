19bff8ee1dccbe2f1622065c0bbde52ff5698e7c
ThePooN <thepoon@osufr.live>
2022-06-30 00:20:35 +0200

    🔥 Remove old pruning code

--- dump_all.sh
+++ dump_all.sh
@@ -3,9 +3,6 @@
 
 set -e
 
-find /var/www/html/*performance*.bz2 -depth -type f -ctime +180 -delete &> /dev/null
-find /var/www/html/*files*.bz2 -depth -type f -ctime +180 -delete &> /dev/null
-
 ./dump_sample_tables.sh osu top 10000
 ./dump_sample_tables.sh osu random 10000
 ./dump_sample_tables.sh taiko top 10000
